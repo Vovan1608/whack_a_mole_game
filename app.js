d16b744ebb6f32e518dccaec07629198b2ca8bf7
Vovan1608 <57871837+Vovan1608@users.noreply.github.com>
2021-06-09 10:31:20 +0300

    added randomSquare

---
+++ app.js
@@ -0,0 +1,13 @@
+const square = document.querySelectorAll('.square');
+const mole = document.querySelector('.mole');
+const timeLeft = document.getElementById('time-left');
+let score = document.getElementById('score');
+
+let result = 0;
+
+function randomSquare() {
+	square.forEach(className => className.classList.remove('mole'));
+
+	let randomPosition = square[Math.floor(Math.random() * 9)];
+	randomPosition.classList.add('mole');
+}
